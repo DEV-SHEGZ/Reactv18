54a99d824c18c400c30b57a970332ca39cc4cc91
DEV-SHEGZ <strexy1998@gmail.com>
2025-04-09 23:09:59 +0100

    still craking it

--- src/listrendering.js
+++ src/listrendering.js
@@ -2,12 +2,18 @@ import { people } from "./Data";
 
 function renderList() {
   const chemists = people.filter((person) => person.profession === "chemist");
-  chemists.map((person) => (
-    <div key={person.id}>
-      <h1> {person.name} </h1>
-      <li> {person.accomplishment} </li>
-    </div>
-  ));
+
+  return (
+    <ul>
+      {chemists.map((person) => (
+        <div key={person.id}>
+          <h1> {person.name} </h1>
+          <li> {person.accomplishment} </li>
+        </div>
+      ))}
+      ;
+    </ul>
+  );
 }
 
 export default renderList;
