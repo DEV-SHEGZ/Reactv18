57101d1a27c191040d1dc8f8e5c74706749a8e85
DEV-SHEGZ <strexy1998@gmail.com>
2025-04-09 22:58:01 +0100

    This is me still trying to wrap  my head around lists rendering in react v18

---
+++ src/Data.js
@@ -0,0 +1,77 @@
+export const people = [
+  {
+    id: 0, // Used in JSX as a key
+    name: "Creola Katherine Johnson",
+    profession: "mathematician",
+    accomplishment: "spaceflight calculations",
+    imageId: "MK3eW3A",
+  },
+  {
+    id: 1, // Used in JSX as a key
+    name: "Mario José Molina-Pasquel Henríquez",
+    profession: "chemist",
+    accomplishment: "discovery of Arctic ozone hole",
+    imageId: "mynHUSa",
+  },
+  {
+    id: 2, // Used in JSX as a key
+    name: "Mohammad Abdus Salam",
+    profession: "physicist",
+    accomplishment: "electromagnetism theory",
+    imageId: "bE7W1ji",
+  },
+  {
+    id: 3, // Used in JSX as a key
+    name: "Percy Lavon Julian",
+    profession: "chemist",
+    accomplishment:
+      "pioneering cortisone drugs, steroids and birth control pills",
+    imageId: "IOjWm71",
+  },
+  {
+    id: 4, // Used in JSX as a key
+    name: "Subrahmanyan Chandrasekhar",
+    profession: "astrophysicist",
+    accomplishment: "white dwarf star mass calculations",
+    imageId: "lrWQx8l",
+  },
+];
+
+/*function render() {
+  const people = [
+    {
+      id: 0,
+      name: "Creola Katherine Johnson",
+      profession: "mathematician",
+    },
+    {
+      id: 1,
+      name: "Mario José Molina-Pasquel Henríquez",
+      profession: "chemist",
+    },
+    {
+      id: 2,
+      name: "Mohammad Abdus Salam",
+      profession: "physicist",
+    },
+    {
+      id: 3,
+      name: "Percy Lavon Julian",
+      profession: "chemist",
+    },
+    {
+      id: 4,
+      name: "Subrahmanyan Chandrasekhar",
+      profession: "astrophysicist",
+    },
+  ];
+
+  const chemists = people.filter((person) => person.profession === "chemist");
+  const listItems = chemists.map((person) => (
+    <li>
+      {person.name} is a {person.profession} and their id is {person.id}
+    </li>
+  ));
+
+  return <ul>{listItems}</ul>;
+}*/
--- src/listrendering.js
+++ src/listrendering.js
@@ -1,34 +1,13 @@
-function render() {
-  const people = [
-    {
-      id: 0,
-      name: "Creola Katherine Johnson",
-      profession: "mathematician",
-    },
-    {
-      id: 1,
-      name: "Mario José Molina-Pasquel Henríquez",
-      profession: "chemist",
-    },
-    {
-      id: 2,
-      name: "Mohammad Abdus Salam",
-      profession: "physicist",
-    },
-    {
-      id: 3,
-      name: "Percy Lavon Julian",
-      profession: "chemist",
-    },
-    {
-      id: 4,
-      name: "Subrahmanyan Chandrasekhar",
-      profession: "astrophysicist",
-    },
-  ];
+import { people } from "./Data";
 
+function renderList() {
   const chemists = people.filter((person) => person.profession === "chemist");
-  console.log(chemists);
+  chemists.map((person) => (
+    <div key={person.id}>
+      <h1> {person.name} </h1>
+      <li> {person.accomplishment} </li>
+    </div>
+  ));
 }
 
-export default render;
+export default renderList;
